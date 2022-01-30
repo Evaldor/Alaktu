d9554e72aa445563a7a89f8808c6c663bf45e236
ASmirnov <gevaldor@gmail.com>
2022-01-30 18:25:12 +0300

    implement client factory

---
+++ AlaktuManager/Client/Services/IPiplineService.cs
@@ -0,0 +1,15 @@
+﻿using AlaktuManager.Shared;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AlaktuManager.Client.Services
+{
+    public interface IPiplineService
+    {
+        Task<IEnumerable<Pipline>> GetPiplines();
+        Task<Pipline> GetPipline(int employeeId);
+        Task<Pipline> AddPipline(Pipline pipline);
+        Task<Pipline> UpdatePipline(Pipline pipline);
+        Task DeletePipline(int Id);
+    }
+}
---
+++ AlaktuManager/Client/Services/PiplineService.cs
@@ -0,0 +1,43 @@
+﻿using AlaktuManager.Shared;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace AlaktuManager.Client.Services
+{
+    public class PiplineService : IPiplineService
+    {
+        private readonly HttpClient httpClient;
+
+        public PiplineService(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+
+        public async Task<IEnumerable<Pipline>> GetPiplines()
+        {
+            return await httpClient.GetFromJsonAsync<IEnumerable<Pipline>>("api/Piplines");
+        }
+
+        public Task<Pipline> AddPipline(Pipline pipline)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeletePipline(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Pipline> GetPipline(int Id)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<Pipline> UpdatePipline(Pipline pipline)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
